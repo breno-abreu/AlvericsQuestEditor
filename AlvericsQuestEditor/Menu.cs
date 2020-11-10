575d1abb94a136038f11f270b45aefdd4d27f1e5
Breno Abreu <breno2601@gmail.com>
2020-11-10 15:45:51 -0300

    Add project files.

---
+++ AlvericsQuestEditor/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace AlvericsQuestEditor
+{
+    /* Classe do menu do editor. Contém os botões que irão engatilhar eventos. */
+    class Menu
+    {
+        // Retângulo para o background do menu
+        private RectangleShape background;
+
+        // Cor do background
+        private Color backgroundColor;
+
+        // Botão para criar novo mundo
+        private Botao botaoNovo;
+
+        // Botão para salvar mundo
+        private Botao botaoSalvar;
+
+        // Botão para carregar mundo
+        private Botao botaoCarregar;
+
+
+        /* Classe aninhada para um botão */
+        class Botao
+        {
+
+        }
+    }
+}
---
+++ AlvericsQuestEditor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace AlvericsQuestEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
