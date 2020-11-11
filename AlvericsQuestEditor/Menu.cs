4dbe75343ae64d0bae4fd3206d24988a3396b3a4
Breno Abreu <breno2601@gmail.com>
2020-11-10 21:51:15 -0300

    Botões adicionados

--- AlvericsQuestEditor/Menu.cs
+++ AlvericsQuestEditor/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
@@ -54,10 +55,20 @@ namespace AlvericsQuestEditor
         // Lista contendo todos os botoes
         private List<Botao> botoes;
 
+        // Lista contendo toda a parte textual do menu
+        private List<Text> labels;
+
+        // Proporção do distanciamento entre botões no eixo X
+        private const float xProporcao = 7f;
+
+        // Distancia entre botões no eixo Y
+        private const float yDistancia = 70f;
+
         /* Enumeração de todas as ações do menu */
         public enum Acao
         {
             Nenhum,
+            IndicarEntidade,
             NovoMundo,
             Salvar,
             Carregar,
@@ -77,14 +88,17 @@ namespace AlvericsQuestEditor
         {
             private Texture textura;
             public Sprite BSprite { get; private set; }
-            public Acao BAcao { get; set; }
+            public Acao BAcao { get; private set; }
+            public int EntidadeID { get; private set; }
 
-            public Botao(Vector2f posicao, string pathTextura, Acao acao)
+            public Botao(Vector2f posicao, string pathTextura, Acao acao, int entidadeID)
             {
                 try
                 {
                     textura = new Texture(pathTextura);
+                    
                     BSprite = new Sprite(textura);
+                    BSprite.Scale = new Vector2f(.3f, .3f);
                 }
                 catch(Exception ex)
                 {
@@ -92,9 +106,10 @@ namespace AlvericsQuestEditor
                     Environment.Exit(0);
                 }
 
+                BSprite.Origin = new Vector2f(BSprite.Texture.Size.X / 2, BSprite.Texture.Size.Y / 2);
                 BAcao = acao;
-                BSprite.Color = Color.White;
                 BSprite.Position = posicao;
+                EntidadeID = entidadeID;
             }
 
             public void AtualizarPosicao(float x, float y)
@@ -107,9 +122,50 @@ namespace AlvericsQuestEditor
             }
         }
 
-        public Menu()
+        public Menu(Vector2f windowSize, View view)
         {
+            // Inicializa o background
+            background = new RectangleShape(view.Size);
+            background.FillColor = new Color(50, 150, 220);
+            background.Position = new Vector2f(view.Center.X - (windowSize.X * 0.3f) / 2f,
+                                               view.Center.Y - windowSize.Y / 2f);
 
+            // Inicializa os botões
+            bNovoMundo = new Botao(GetPosicaoBotao(windowSize, view, -1, 1), @"MenuIcons\new.png", Acao.NovoMundo, -1);
+            bSalvar = new Botao(GetPosicaoBotao(windowSize, view, 0, 1), @"MenuIcons\save.png", Acao.Salvar, -1);
+            bCarregar = new Botao(GetPosicaoBotao(windowSize, view, 1, 1), @"MenuIcons\archive.png", Acao.Carregar, -1) ;
+            bAdicionarObjeto = new Botao(GetPosicaoBotao(windowSize, view, -1, 2), @"MenuIcons\pencil.png", Acao.AdicionarObjeto, -1);
+            bExcluirObjeto = new Botao(GetPosicaoBotao(windowSize, view, 0, 2), @"MenuIcons\eraser.png", Acao.ExcluirObjeto, -1);
+            bGerenciarConexao = new Botao(GetPosicaoBotao(windowSize, view, 1, 2), @"MenuIcons\magnet.png", Acao.GerenciarConexao, -1);
+            bInicioAtivacaoSoma = new Botao(GetPosicaoBotao(windowSize, view, 1, 3), @"MenuIcons\right.png", Acao.GerenciarConexao, -1);
+            bInicioAtivacaoSub = new Botao(GetPosicaoBotao(windowSize, view, -1, 3), @"MenuIcons\left.png", Acao.GerenciarConexao, -1);
+            bTempoAtivadoSoma = new Botao(GetPosicaoBotao(windowSize, view, 1, 4), @"MenuIcons\right.png", Acao.GerenciarConexao, -1);
+            bTempoAtivadoSub = new Botao(GetPosicaoBotao(windowSize, view, -1, 4), @"MenuIcons\left.png", Acao.GerenciarConexao, -1);
+            bTempoEntreAtivacoesSoma = new Botao(GetPosicaoBotao(windowSize, view, 1, 5), @"MenuIcons\right.png", Acao.GerenciarConexao, -1);
+            bTempoEntreAtivacoesSub = new Botao(GetPosicaoBotao(windowSize, view, -1, 5), @"MenuIcons\left.png", Acao.GerenciarConexao, -1);
+
+            botoes = new List<Botao>() 
+            { 
+                bNovoMundo, 
+                bSalvar, 
+                bCarregar, 
+                bAdicionarObjeto, 
+                bExcluirObjeto, 
+                bGerenciarConexao,
+                bInicioAtivacaoSoma,
+                bInicioAtivacaoSub,
+                bTempoAtivadoSoma,
+                bTempoAtivadoSub,
+                bTempoEntreAtivacoesSoma,
+                bTempoEntreAtivacoesSub,
+            };
+        }
+
+        private Vector2f GetPosicaoBotao(Vector2f windowSize, View view, int coluna, int linha)
+        {
+            float posColuna = view.Center.X + coluna * (view.Size.X / xProporcao);
+            float posLinha = view.Center.Y - windowSize.Y / 2 + yDistancia * linha;
+            return new Vector2f(posColuna, posLinha);
         }
 
         public Acao BotaoPressionado(float x, float y)
@@ -117,13 +173,56 @@ namespace AlvericsQuestEditor
             // Encontra o botao que foi pressionado e o ativa
             foreach(Botao botao in botoes)
             {
-                if (x >= botao.BSprite.Position.X && x <= botao.BSprite.Texture.Size.X &&
-                    y >= botao.BSprite.Position.Y && y <= botao.BSprite.Texture.Size.Y)
+                // Variáveis recebem os seguintes atributos de um botão: 
+                // posição em x, posição em y, x mais o comprimento, y mais a altura
+
+                /* ----------------------------------------------------------------*/
+                float bx = botao.BSprite.Position.X;
+                float by = botao.BSprite.Position.Y;
+                float bcomprimento = bx + botao.BSprite.Texture.Size.X;
+                float baltura = by + botao.BSprite.Texture.Size.Y;
+                /* ----------------------------------------------------------------*/
+
+                if (x >= bx && x <= bcomprimento &&
+                    y >= by && y <= baltura)
                     return botao.BAcao;
             }
 
-            // Caso nenhum botão tenha sido pressionado, retorna false
+            // Caso nenhum botão tenha sido pressionado, retorna a acao Nenhum
             return Acao.Nenhum;
+        }
+
+        public void Desenhar(RenderWindow window)
+        {
+            window.Draw(background);
+
+            foreach (Botao botao in botoes)
+                window.Draw(botao.BSprite);
+
+            /*foreach (Text label in labels)
+                window.Draw(label);*/
+        }
+
+        public void RedimensionarMenu(Vector2f windowSize, View view)
+        {
+            // Redimensiona o background
+            background.Size = view.Size;
+            background.Position = new Vector2f(view.Center.X - (windowSize.X * 0.3f) / 2f,
+                                               view.Center.Y - windowSize.Y / 2f);
+
+            // Determina a nova posição dos botões
+            bNovoMundo.BSprite.Position = GetPosicaoBotao(windowSize, view, -1, 1);
+            bSalvar.BSprite.Position = GetPosicaoBotao(windowSize, view, 0, 1);
+            bCarregar.BSprite.Position = GetPosicaoBotao(windowSize, view, 1, 1);
+            bAdicionarObjeto.BSprite.Position = GetPosicaoBotao(windowSize, view, -1, 2);
+            bExcluirObjeto.BSprite.Position = GetPosicaoBotao(windowSize, view, 0, 2);
+            bGerenciarConexao.BSprite.Position = GetPosicaoBotao(windowSize, view, 1, 2);
+            bInicioAtivacaoSoma.BSprite.Position = GetPosicaoBotao(windowSize, view, 1, 3);
+            bInicioAtivacaoSub.BSprite.Position = GetPosicaoBotao(windowSize, view, -1, 3);
+            bTempoAtivadoSoma.BSprite.Position = GetPosicaoBotao(windowSize, view, 1, 4);
+            bTempoAtivadoSub.BSprite.Position = GetPosicaoBotao(windowSize, view, -1, 4);
+            bTempoEntreAtivacoesSoma.BSprite.Position = GetPosicaoBotao(windowSize, view, 1, 5);
+            bTempoEntreAtivacoesSub.BSprite.Position = GetPosicaoBotao(windowSize, view, -1, 5);
         }
     }
 }
--- AlvericsQuestEditor/Program.cs
+++ AlvericsQuestEditor/Program.cs
@@ -10,7 +10,92 @@ namespace AlvericsQuestEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Editor editor = new Editor();
+            editor.Executar();
+        }
+    }
+
+    class Editor
+    {
+        private RenderWindow window;
+        private View viewMundo;
+        private View viewMenu;
+        private Menu menu;
+        private const uint comprimentoInicialTela = 1600;
+        private const uint alturaInicialTela = 900;
+        public Editor()
+        {
+            // Inicializa a janela do programa
+            window = new RenderWindow(new VideoMode(comprimentoInicialTela, alturaInicialTela), 
+                                      "Alveric's Quest Editor", Styles.Default);
+
+            // Cria uma view para o mundo
+            viewMundo = new View();
+            // Seleciona a fração da tela ocupada por essa view
+            viewMundo.Viewport = new FloatRect(0f, 0f, 0.7f, 1f);
+            viewMundo.Size = new Vector2f(window.Size.X * 0.7f, window.Size.Y);
+
+            // Cria uma view para o menu
+            viewMenu = new View();
+            // Seleciona a fração da tela ocupada por essa view
+            viewMenu.Viewport = new FloatRect(0.7f, 0f, 0.3f, 1f);
+            viewMenu.Size = new Vector2f(window.Size.X * 0.3f, window.Size.Y);
+
+            // Inicializa o menu
+            menu = new Menu((Vector2f)window.Size, viewMenu);
+
+            // Tenta carregar o ícone da janela, caso não consigo o programa é finalizado
+            try
+            {
+                Image icone = new Image(@"MenuIcons\window_icon.png");
+                window.SetIcon(icone.Size.X, icone.Size.Y, icone.Pixels);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+
+            /* Inclui um método para os event handlers: */
+            // Método chamado quando ó botão de finalizar o programa é pressionado
+            window.Closed += Window_Close;
+
+            // Método chamado quando a tela é redimensionada
+            window.Resized += Window_Resized;
+        }
+
+        public void Executar()
+        {
+            while (window.IsOpen)
+            {
+                // Trata os eventos da janela
+                window.DispatchEvents();
+
+                // Muda para a viewMenu e desenha o menu na tela
+                window.SetView(viewMenu);
+                menu.Desenhar(window);
+
+                // Executa a janela
+                window.Display();
+
+                // Limpa a janela
+                window.Clear(Color.White);
+            }
+        }
+
+        /* Método chamado quando a tela é redimensionada */
+        private void Window_Resized(object sender, EventArgs e)
+        {
+            // Redimensionar menu
+            viewMenu.Size = new Vector2f(window.Size.X * 0.3f, window.Size.Y);
+            menu.RedimensionarMenu((Vector2f)window.Size, viewMenu);
+        }
+
+        /* Método chamado quando o botão 'fechar janela' é pressionado */
+        private void Window_Close(object sender, EventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+            window.Close();
         }
     }
 }
